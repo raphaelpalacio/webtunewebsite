c22e0a3e6f24ce60c78155b9fe6692c1cb26df11
Raphael Palacio <54969177+raphaelpalacio@users.noreply.github.com>
2024-03-30 09:59:53 -0400

    added navbar to the react app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import AboutUs from './pages/aboutus';
@@ -9,19 +10,33 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <Link to="/about-us">About Us</Link> | 
-          <Link to="/">Web Tune</Link> |  {/* Updated path */}
-          <Link to="/why-seo">Why SEO</Link>
+        { /* Boostrap Navbar */ }
+        <nav className = "navbar navbar-expand-lg navbar-light bg-light">
+          <Link className = "navbar-brand" to = "/">WebTune</Link>
+          <button className = "navbar-toggler" type = "button" data-toggle = "collapse" data-target = "#navbarNav" aria-controls = "navbarNav" aria-expanded = "false" aria-label = "Toggle navigation">
+            <span className = "navbar-toggler-icon"></span>
+          </button>
+          <div className = "collapse navbar-collapse" id = "navbarNav">
+            <ul className = "navbar-nav">
+              <li className = "nav-item active">
+                <Link className = "nav-link" to = "/">WebTune</Link>
+              </li>
+              <li className = "nav-item">
+                <Link className = "nav-link" to = "/why-seo">Why SEO</Link>
+              </li>
+              <li className = "nav-item">
+                <Link className = "nav-link" to = "/about-us">About Us</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
-        
-        <Routes>
-          <Route path="/" element={<WebTune />} /> {/* Set WebTune as the home page */}
 
-          <Route path="/about-us" element={<AboutUs />} />
-          
+        {/* Routes */}
+
+        <Routes>
+          <Route path="/" element={<WebTune />} /> {/* Home page */}
           <Route path="/why-seo" element={<WhySEO />} />
-          {/* Remove the duplicate route for AboutUs */}
+          <Route path="/about-us" element={<AboutUs />} />
         </Routes>
       </div>
     </Router>
